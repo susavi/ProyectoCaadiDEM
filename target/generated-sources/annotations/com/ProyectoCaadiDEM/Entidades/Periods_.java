84662ceb36fe3959f64291fa2261599f5f6ae909
frodo <frodo@frodo>
2018-09-27 00:26:15 -0500

    haciendole un olverlaypanel al boton de iniciar sesion

--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Periods_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Periods_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-09-26T10:26:57")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-09-26T17:07:11")
 @StaticMetamodel(Periods.class)
 public class Periods_ { 
 
