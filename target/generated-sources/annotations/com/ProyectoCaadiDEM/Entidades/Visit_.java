e13a998e5f9baa04844f99f059c2f3513712ec26
arthyom <a.gonzalezgavina@ugto.mx>
2018-10-03 23:11:12 -0500

    Mejorando el aspecto grafico

--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Visit_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Visit_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-02T15:07:16")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-03T14:27:09")
 @StaticMetamodel(Visit.class)
 public class Visit_ { 
 
