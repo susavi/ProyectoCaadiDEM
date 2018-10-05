00e1ce04bb5deb51d94a31f77294675c2c045994
arthyom <a.gonzalezgavina@ugto.mx>
2018-10-04 23:49:39 -0500

    Añadiendo CSS a los cruds

--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Groups_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Groups_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-03T14:27:09")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-10-04T21:25:22")
 @StaticMetamodel(Groups.class)
 public class Groups_ { 
 
