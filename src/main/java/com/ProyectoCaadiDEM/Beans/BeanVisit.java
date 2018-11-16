3be2a308c842fb0f569a14da99c574a004a8e46b
arthyom <a.gonzalezgavina@ugto.mx>
2018-11-16 01:46:58 -0600

    ?!

--- src/main/java/com/ProyectoCaadiDEM/Beans/BeanVisit.java
+++ src/main/java/com/ProyectoCaadiDEM/Beans/BeanVisit.java
@@ -433,11 +433,15 @@ public class BeanVisit implements Serializable {
         System.out.println("----------------------" +ips);
 
         //|| !"0:0:0:0:0:0:0:1".equals(ip)
+        
         if (!"0:0:0:0:0:0:0:1".equals(ip)) 
             if( p.length == 1)
                 FacesContext.getCurrentInstance().getExternalContext().redirect(s);
             else
                 FacesContext.getCurrentInstance().getExternalContext().redirect("404.xhtml");
+        
+        
+    
         
         /*
         if (!ips.equals(ip)) 
