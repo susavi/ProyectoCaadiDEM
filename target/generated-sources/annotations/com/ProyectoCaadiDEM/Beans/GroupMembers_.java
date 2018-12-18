ead403a95fa0dd1584d56bd5bd60196a452e85d8
arthyom <a.gonzalezgavina@ugto.mx>
2018-12-17 23:46:39 -0600

    mejorando el aspecto de carga

--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Beans/GroupMembers_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Beans/GroupMembers_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-11-28T23:13:06")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-12-17T23:42:10")
 @StaticMetamodel(GroupMembers.class)
 public class GroupMembers_ { 
 
--- target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Groups_.java
+++ target/generated-sources/annotations/com/ProyectoCaadiDEM/Entidades/Groups_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-11-28T23:13:06")
+@Generated(value="EclipseLink-2.6.1.v20150605-rNA", date="2018-12-17T23:42:10")
 @StaticMetamodel(Groups.class)
 public class Groups_ { 
 
